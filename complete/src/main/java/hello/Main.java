441b80efe0befef6ee1ec6eb3bbdb3e864409e7b
Chris Beams <cbeams@gopivotal.com>
2013-05-21 13:18:57 +0200

    Simplify complete version of Main.java
     - Remove unused calls
     - Inline the #iterateTweets method
     - Remove unnecessary throws clause

--- complete/src/main/java/hello/Main.java
+++ complete/src/main/java/hello/Main.java
@@ -1,32 +1,24 @@
 package hello;
 
 import org.codehaus.jackson.JsonNode;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public class Main {
 
-    public static void main(String args[]) throws Throwable {
+    public static void main(String args[]) {
 
         String searchUrl = "http://search.twitter.com/search.json?q={query}";
-        String searchQueryPathVariableValue = "@gopivotal";
+        String query = "@gopivotal";
 
         RestTemplate restTemplate = new RestTemplate();
 
-        String bodyOfResponse = restTemplate.getForObject(searchUrl, String.class, searchQueryPathVariableValue);
+        JsonNode rootNode = restTemplate.getForObject(
+            searchUrl, JsonNode.class, query);
 
-        JsonNode rootNode = restTemplate.getForObject(searchUrl, JsonNode.class, searchQueryPathVariableValue);
-
-        ResponseEntity<JsonNode> response = restTemplate.getForEntity(searchUrl, JsonNode.class, searchQueryPathVariableValue);
-
-        HttpHeaders httpHeaders = response.getHeaders();
-
-    }
-
-    static void iterateTweets(JsonNode jsonNode) {
-        for (JsonNode result : jsonNode.get("results"))
-            System.out.println(String.format("Tweet: @%s said \"%s\"", result.get("from_user").getTextValue(), result.get("text").getTextValue()));
-        System.out.println();
+        for (JsonNode result : rootNode.get("results")) {
+            System.out.println(
+                String.format("Tweet: @%s said \"%s\"",
+                    result.get("from_user").getTextValue(), result.get("text").getTextValue()));
+        }
     }
 }
