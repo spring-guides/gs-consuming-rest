e3a18a9bfbf5c7aacf909a203a5ae78a6f197a8c
钟复之 <125626748@users.noreply.github.com>
2015-07-30 11:08:36 +0800

    Update Value.java
    Getter/Setter and toString

--- complete/src/main/java/hello/Value.java
+++ complete/src/main/java/hello/Value.java
@@ -9,5 +9,20 @@ public class Value {
 
     private Long id;
     private String quote;
-
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getQuote() {
+		return quote;
+	}
+	public void setQuote(String quote) {
+		this.quote = quote;
+	}
+	@Override
+	public String toString() {
+		return "Value [id=" + id + ", quote=" + quote + "]";
+	}
 }
