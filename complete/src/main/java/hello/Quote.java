c581f45d5e805511b6e03341aeeb900502c43bf5
钟复之 <125626748@users.noreply.github.com>
2015-07-30 11:09:12 +0800

    Update Quote.java
    Getter/Setter and toString

--- complete/src/main/java/hello/Quote.java
+++ complete/src/main/java/hello/Quote.java
@@ -9,5 +9,24 @@ public class Quote {
 
     private String type;
     private Value value;
+	public String getType() {
+		return type;
+	}
 
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Value getValue() {
+		return value;
+	}
+
+	public void setValue(Value value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "Quote [type=" + type + ", value=" + value + "]";
+	}
 }
