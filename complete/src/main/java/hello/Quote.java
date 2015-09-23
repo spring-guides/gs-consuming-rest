eb443a5d8d576af23a7b81b0f8461faa5ce3594e
Greg Turnquist <gturnquist@pivotal.io>
2015-09-23 12:08:45 -0500

    Delombok the guide
    Removed lombok to simplify the code.
    
    Resolves #13

--- complete/src/main/java/hello/Quote.java
+++ complete/src/main/java/hello/Quote.java
@@ -1,13 +1,29 @@
 package hello;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
 
-@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Quote {
 
     private String type;
     private Value value;
 
+    public Quote() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
 }
--- complete/src/main/java/hello/Value.java
+++ complete/src/main/java/hello/Value.java
@@ -1,13 +1,30 @@
 package hello;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
 
-@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Value {
 
     private Long id;
     private String quote;
+
+    public Value() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getQuote() {
+        return this.quote;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
 
 }
