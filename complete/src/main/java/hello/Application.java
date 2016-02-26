3f97c21b76dfe2e4a1a9e895916c683c297516fc
Thomas F. Düllmann <duellmtk@studi.informatik.uni-stuttgart.de>
2016-02-26 15:52:25 +0100

    Removed SpringBootApplication and CommandLineRunner from Application class. So it does not start a tomcat instance on its own.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -2,22 +2,13 @@ package hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String args[]) {
-        SpringApplication.run(Application.class);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
         RestTemplate restTemplate = new RestTemplate();
         Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
         log.info(quote.toString());
