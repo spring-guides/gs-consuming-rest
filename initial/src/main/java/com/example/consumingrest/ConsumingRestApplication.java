116fe92f5950dc4f1140520dd5f71b10bfe0ab64
Jay Bryant <jbryant@pivotal.io>
2019-08-26 11:53:31 -0500

    Incorporated Greg Turnquist's review comments
    Greg Turnquist did a review and caught a few things. I have fixed those things. Thanks, Greg. :)

---
+++ initial/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -0,0 +1,18 @@
+package com.example.consumingrest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ConsumingRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestApplication.class, args);
+	}
+}
