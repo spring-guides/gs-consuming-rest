ed552b88e8de2274c026c7c6c30347dcc608e884
Jay Bryant <jbryant@pivotal.io>
2019-10-22 13:58:16 -0500

    Upgraded to Spring Boot 2.2
    I upgraded the build files to Spring Boot 2.2.0, which necessitated changing the tests to use JUnit 5 and replacing the Gradle wrappers. I also noticed that I hadn't change the package of the /complete test path, so I did that, too.

--- complete/src/test/java/com/example/consumingrest/ConsumingRestApplicationTest.java
+++ complete/src/test/java/com/example/consumingrest/ConsumingRestApplicationTest.java
@@ -18,17 +18,15 @@ package com.example.consumingrest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTest {
-	
+public class ConsumingRestApplicationTest {
+
 	@Autowired
 	private RestTemplate restTemplate;
 
