058074c84ccc31cb7ba7f83845a1be8caea5293a
Greg Turnquist <gturnquist@pivotal.io>
2019-11-26 13:08:01 -0600

    Switch to tabs, and adjust tabsizing in asciidoctor.

--- complete/src/main/java/com/example/consumingrest/Quote.java
+++ complete/src/main/java/com/example/consumingrest/Quote.java
@@ -5,33 +5,33 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Quote {
 
-    private String type;
-    private Value value;
+	private String type;
+	private Value value;
 
-    public Quote() {
-    }
+	public Quote() {
+	}
 
-    public String getType() {
-        return type;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public void setType(String type) {
+		this.type = type;
+	}
 
-    public Value getValue() {
-        return value;
-    }
+	public Value getValue() {
+		return value;
+	}
 
-    public void setValue(Value value) {
-        this.value = value;
-    }
+	public void setValue(Value value) {
+		this.value = value;
+	}
 
-    @Override
-    public String toString() {
-        return "Quote{" +
-                "type='" + type + '\'' +
-                ", value=" + value +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Quote{" +
+				"type='" + type + '\'' +
+				", value=" + value +
+				'}';
+	}
 }
--- complete/src/main/java/com/example/consumingrest/Value.java
+++ complete/src/main/java/com/example/consumingrest/Value.java
@@ -5,33 +5,33 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Value {
 
-    private Long id;
-    private String quote;
+	private Long id;
+	private String quote;
 
-    public Value() {
-    }
+	public Value() {
+	}
 
-    public Long getId() {
-        return this.id;
-    }
+	public Long getId() {
+		return this.id;
+	}
 
-    public String getQuote() {
-        return this.quote;
-    }
+	public String getQuote() {
+		return this.quote;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setQuote(String quote) {
-        this.quote = quote;
-    }
+	public void setQuote(String quote) {
+		this.quote = quote;
+	}
 
-    @Override
-    public String toString() {
-        return "Value{" +
-                "id=" + id +
-                ", quote='" + quote + '\'' +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Value{" +
+				"id=" + id +
+				", quote='" + quote + '\'' +
+				'}';
+	}
 }
