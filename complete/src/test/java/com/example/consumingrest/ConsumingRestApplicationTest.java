710e5e666bc7e5c6b82db00223a56e552e2a7185
Robert McNees <rmcnees@vmware.com>
2023-09-26 08:17:18 -0400

    Disable CommandLineRunner from executing during Unit tests

--- complete/src/test/java/com/example/consumingrest/ConsumingRestApplicationTest.java
+++ complete/src/test/java/com/example/consumingrest/ConsumingRestApplicationTest.java
@@ -22,9 +22,11 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class ConsumingRestApplicationTest {
 
 	@Autowired
